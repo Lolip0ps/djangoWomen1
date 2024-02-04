a299da5534273da1ac9faad61482824dbd85a843
Илья <ilalogunov6@gmail.com>
2024-02-04 16:13:52 +0300

    начало 7 урока

---
+++ sitewomen/women/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('cats/<int:cat_id>/', views.category),
+    path('cats/<slug:cat_slug>/', views.category_by_slug),
+]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the")
+
+
+def category(request, cat_id):
+    return HttpResponse(f'<h1>Category</h1><p>id: {cat_id}</p>')
+
+
+def category_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Category by slug</h1><p>slug: {cat_slug}</p>')
