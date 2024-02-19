120874b14f4c915be0f233eaadf6fb2495933229
Lolip0ps <78905754+Lolip0ps@users.noreply.github.com>
2024-02-19 16:47:14 +0300

    13

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'addpage'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
 
 data_db = [
     {'id': 1, 'title': 'Боба', 'content': 'Биография бобы', "is_published": True},
     {'id': 2, 'title': 'Биба', 'content': 'Биография Бибы', "is_published": False},
-    {'id': 2, 'title': 'Пупа', 'content': 'Биография Пупы', "is_published": True},
+    {'id': 3, 'title': 'Пупа', 'content': 'Биография Пупы', "is_published": True},
 ]
 
 
@@ -37,3 +42,15 @@ def page_not_found(request, exception):
 
 def show_post(request, post_id):
     return HttpResponse(f'Отображение статьи с id = {post_id}')
+
+
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
